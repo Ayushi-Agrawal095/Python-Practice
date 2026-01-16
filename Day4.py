7d0d71d49c2bdb5fd513b6074a5cf2bab4b64567
Ayushi-Agrawal095 <ayushiagrawal77777@gmail.com>
2026-01-16 09:30:25 +0530

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,15 @@
+#Print
+print("hello world")
+#variabe and print
+name = "Ayushi"
+age= 22
+print(name)
+print(age)
+#basic math 
+a=10
+b=1
+print(a+b) # addition
+print(a-b) # substraction
+print(a*b) # multiplication
+print(a/b) # division
+
